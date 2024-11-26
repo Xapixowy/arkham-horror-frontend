460c1f3ce5c6996b17d09cca743d468d20be7a7c
Jakub "Xapixowy" Chodziński <xapixowy@gmail.com>
2024-11-26 23:23:15 +0100

    start to work on game-sessions page

---
+++ src/app/pages/admin/game-sessions-page/game-sessions-page.component.ts
@@ -0,0 +1,13 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-game-sessions-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-sessions-page.component.html',
+  styleUrl: './game-sessions-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GameSessionsPageComponent {
+
+}
